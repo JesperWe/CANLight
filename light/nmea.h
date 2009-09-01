9a31dcffa892c048474b47f74f35116faa8015e9
jesper@seventhwave.se <jesper@seventhwave.se@ab27f7e6-96f5-11de-a465-5fc2cdd6756b>
2009-09-01 12:57:52 +0000

    Initial experimental code
    git-svn-id: https://can-light.googlecode.com/svn/trunk@3 ab27f7e6-96f5-11de-a465-5fc2cdd6756b

---
+++ light/nmea.h
@@ -0,0 +1,156 @@
+/*
+ * nmea.h
+ *
+ *  Created on: 2009-jun-01
+ *      Author: Jesper W
+ */
+
+#ifndef NMEA_H_
+#define NMEA_H_
+
+#define __debug_pulse(pin)		pin = 1; NOP; pin = 0; NOP;
+
+// Return Codes.
+
+#define nmea_SUCCESS			0
+#define nmea_QUEUEEMPTY			1
+#define nmea_QUEUEFULL			1
+#define nmea_CANNOTRECEIVE		2
+#define nmea_CANNOTTRANSMIT		2
+#define nmea_PARAMERROR			3
+#define nmea_TRANSMITTER_BUSY	4
+
+
+// Parameter Group Names
+
+#define nmea_LIGHTING_DATA			65088
+#define nmea_LIGHTING_COMMAND		65089
+#define nmea_MAINTAIN_POWER			65095
+
+
+// NMEA2000 CA NAME basics for a marine electrical lighting appliance.
+
+#define nmea_STARTING_ADDRESS		128
+#define nmea_ARBITRARY_ADDRESS		1	// Yes we can!
+#define nmea_INDUSTRY_GROUP			4	// Marine = 4
+#define nmea_VEHICLE_SYSTEM_INST	0
+
+#define nmea_VEHICLE_SYSTEM			30	// Power Management and Lighting Systems
+
+#define nmea_FUNCTION_SWITCH		130
+#define nmea_FUNCTION_LOAD			140
+
+#define nmea_FUNCTION_INSTANCE		0
+#define nmea_ECU_INSTANCE			0
+
+#define nmea_MANUFACTURER_CODE		2000		// 11 bits
+#define nmea_IDENTITY_NUMBER		471101		// 21 bits
+
+#define nmea_GLOBAL_ADDRESS			255
+#define nmea_NULL_ADDRESS			254
+
+#define nmea_NORMAL_MSG				0b00
+#define nmea_REMOTE_TX_MSG			0b10
+#define nmea_STANDARD_ID			0b00
+#define nmea_EXTENDED_ID			0b01
+
+// Message and Message Buffer sizes.
+
+#define nmea_MSG_HEADER_LENGTH		5
+#define nmea_MSG_BUFFER_WORDS		8
+#define nmea_MSG_BUFFER_BYTES		2*nmea_MSG_BUFFER_WORDS
+
+#define nmea_NO_MSG_BUFFERS			4
+#define nmea_MSG_DATA_BYTES			8
+
+#define nmea_RX_QUEUE_SIZE			3
+#define nmea_TX_QUEUE_SIZE			3
+
+#define nmea_TX_IN_PROGRESS			C1TR01CONbits.TXREQ0
+
+typedef unsigned short nmea_MsgBuffer_t[nmea_MSG_BUFFER_WORDS];
+
+
+union nmea_PGN_UNION {
+	struct {
+		unsigned Priority			: 3;
+		unsigned _Reserved			: 1;
+		unsigned Datapage			: 1;
+		unsigned char PDUFormat;
+		unsigned char PDUSpecific;
+		unsigned short PGN;
+		unsigned char SourceAddress;
+		unsigned char bytes;
+		unsigned char data[8];
+	};
+};
+
+typedef union nmea_PGN_UNION nmea_PGN_t;
+
+
+union nmea_FLAGS_UNION {
+	struct {
+		unsigned CannotClaimAddress					: 1;
+		unsigned WaitingForAddressClaimContention	: 1;
+		unsigned GettingCommandedAddress			: 1;
+		unsigned GotFirstDataPacket					: 1;
+		unsigned ReceivedMessagesDropped			: 1;
+	};
+	unsigned char all;
+};
+
+typedef union nmea_FLAGS_UNION nmea_FLAG;
+
+
+typedef	struct {
+	unsigned vehicle_syst_inst	: 4;
+	unsigned industry_group		: 3;
+	unsigned arbitrary_addr		: 1;
+
+	unsigned reserved			: 1;
+	unsigned vehicle_syst		: 7;
+
+	unsigned char function;
+
+	unsigned ecu_inst			: 3;
+	unsigned function_inst		: 5;
+
+	unsigned long identity		: 21;
+	unsigned manufacturer		: 11;
+} nmea_CA_NAME_t;
+
+
+
+
+//---------------------------------------------------------------------------------------------
+// Globals.
+
+extern unsigned char 	nmea_CA_Address;
+extern nmea_FLAG    	nmea_Flags;
+extern unsigned char	RXQueueCount;
+extern nmea_MsgBuffer_t	nmea_MsgBuf[nmea_NO_MSG_BUFFERS] __attribute__((space(dma)));
+extern nmea_CA_NAME_t 	nmea_CA_Name;
+extern nmea_MsgBuffer_t	inMessage;
+extern nmea_PGN_t 		inPGN, outPGN;
+extern nmea_MsgBuffer_t	nmea_TxQueue[nmea_NO_MSG_BUFFERS];
+extern unsigned char	nmea_TxQueueHead;
+extern unsigned char	nmea_TxQueueTail;
+extern unsigned char	nmea_TxQueueFull;
+
+//---------------------------------------------------------------------------------------------
+// Prototypes.
+
+void nmea_Initialize();
+
+void nmea_MakePGN( nmea_PGN_t *pgn,
+		unsigned short pdn_priority,
+		unsigned short pgn_no,
+		unsigned short msg_bytes );
+
+unsigned char nmea_SendMessage( nmea_PGN_t *pgn );
+
+void nmea_ControllerMode( unsigned char mode );
+
+void nmea_GetReceivedPGN( nmea_PGN_t *pgn );
+
+#endif /* NMEA_H_ */
