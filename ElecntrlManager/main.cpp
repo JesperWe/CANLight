1b966932e93e52ac50be2bd6d9cec641dee9e582
jesper.weissglas <jesper.weissglas@ab27f7e6-96f5-11de-a465-5fc2cdd6756b>
2010-03-28 07:34:51 +0000

    Messed things up while moving project around...
    git-svn-id: https://can-light.googlecode.com/svn/trunk@104 ab27f7e6-96f5-11de-a465-5fc2cdd6756b

--- ElecntrlManager/main.cpp
+++ ElecntrlManager/main.cpp
@@ -1,12 +1,19 @@
-#include "elecntrlmanager.h"
+#include <QtGui/QApplication>
+#include <QDesktopWidget>
 
-#include <QtGui>
-#include <QApplication>
+#include "mainwindow.h"
 
 int main(int argc, char *argv[])
 {
-    QApplication a(argc, argv);
-    ElecntrlManager w;
-    w.show();
-    return a.exec();
+	QApplication a(argc, argv);
+	MainWindow w;
+
+	//QDesktopWidget *desktop = QApplication::desktop();
+
+	//int screenWidth = desktop->width();
+	//int screenHeight = desktop->height();
+
+	w.show();
+
+	return a.exec();
 }
